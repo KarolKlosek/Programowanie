bf8fc4da42603ef2a48e132bbcd28b21044fb638
Karol Kłosek <karolklosek96@gmail.com>
2022-12-06 12:15:20 +0100

    delete tab

--- zpliku.py
+++ zpliku.py
@@ -1,13 +1,13 @@
 
 with open('readme.txt', 'r') as fp:
     file = fp.readlines()
-    nrLine = []
-    nrLine.append(int(input("Enter line number:")))
-    currentLine = []
-    lenFile = len(file)
-    for i, line in enumerate (file):
-        if i in nrLine:
-            currentLine.append(line.strip())
-        elif i > lenFile:
-            break
-    print(nrLine, currentLine)
+nrLine = []
+nrLine.append(int(input("Enter line number:")))
+currentLine = []
+lenFile = len(file)
+for i, line in enumerate (file):
+    if i in nrLine:
+        currentLine.append(line.strip())
+    elif i > lenFile:
+        break
+print(nrLine, currentLine)
