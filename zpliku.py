8694fa811bb22f47323a4aa15f1910da684c119a
Karol Kłosek <karolklosek96@gmail.com>
2022-12-12 21:57:56 +0100

    adding new text file dont work xD

--- zpliku.py
+++ zpliku.py
@@ -1,5 +1,34 @@
+print("choose a file ")
+print("1 - Lorem Ipsum")
+print("2 - Gaius Iulius Gaii")
+print("3 - Imperium Romanum")
+print("4 - Publius Vergilius Maro")
 
-with open('readme.txt', 'r') as fp:
+choseeFile = input("Enter File Number: ")
+
+print(choseeFile)
+chosenFile = []
+
+
+if choseeFile == 1:
+    print("1")
+    chosenFile = "FileToRead/1.txt"
+elif choseeFile == 2:
+    print("2")
+    chosenFile = "FileToRead/2.txt"
+elif choseeFile == 3:
+    print("3")
+    chosenFile = "FileToRead/3.txt"
+elif choseeFile == 4:
+    print("4")
+    chosenFile = "FileToRead/4.txt"
+else:
+    print("Wrong number")
+
+
+asdf = "'FileToRead/1.txt'"
+
+with open(asdf, 'r') as fp:
     file = fp.readlines()
 nrLine = []
 nrLine.append(int(input("Enter line number:")))
