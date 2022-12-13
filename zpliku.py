c10c00cf4c768fdceece35668e773487075cef78
Karol Kłosek <karolklosek96@gmail.com>
2022-12-13 20:52:06 +0100

    Chose file  work

--- zpliku.py
+++ zpliku.py
@@ -4,31 +4,24 @@ print("2 - Gaius Iulius Gaii")
 print("3 - Imperium Romanum")
 print("4 - Publius Vergilius Maro")
 
-choseeFile = input("Enter File Number: ")
-
-choseeFile = int(choseeFile)
-
-
+choseeFile = int(input("Enter File Number: "))
 
 if choseeFile == 1:
     print("1")
-    chosenFile = "FileToRead/1.txt"
+    chosenFile = "FilesToRead/1.txt"
 elif choseeFile == 2:
     print("2")
-    chosenFile = "FileToRead/2.txt"
+    chosenFile = "FilesToRead/2.txt"
 elif choseeFile == 3:
     print("3")
-    chosenFile = "FileToRead/3.txt"
+    chosenFile = "FilesToRead/3.txt"
 elif choseeFile == 4:
     print("4")
-    chosenFile = "FileToRead/4.txt"
+    chosenFile = "FilesToRead/4.txt"
 else:
     print("Wrong number")
 
-
-asdf = "'FileToRead/1.txt'"
-
-with open(asdf, 'r') as fp:
+with open("{}".format(chosenFile), 'r') as fp:
     file = fp.readlines()
 nrLine = []
 nrLine.append(int(input("Enter line number:")))
